8295a0ce319ab9e14fc69809ab00deaca1da18f1
myeraug <140523979+myeraug@users.noreply.github.com>
2024-10-13 18:17:00 +0800

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+# 100 Days of Code - The Complete Python Pro Bootcamp
+# day 01 project
+# band name generator
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?: \n")
+pet = input("What's your pet's name?: \n")
+
+print("Your band name could be " + city + " " + pet)
